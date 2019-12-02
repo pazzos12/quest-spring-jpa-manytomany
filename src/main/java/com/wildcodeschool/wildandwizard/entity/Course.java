205ee55f154b9440905c2053b37263af9b3f408c
pattoche12 <patange612@gmail.com>
2019-12-02 01:41:21 +0100

    JPA manyTomany done

--- src/main/java/com/wildcodeschool/wildandwizard/entity/Course.java
+++ src/main/java/com/wildcodeschool/wildandwizard/entity/Course.java
@@ -1,9 +1,14 @@
 package com.wildcodeschool.wildandwizard.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
 
 @Entity
 public class Course {
@@ -12,6 +17,9 @@ public class Course {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    
+    @ManyToMany(mappedBy = "courses")
+    private List<Wizard> wizards = new ArrayList<>();
 
     public Course() {
     }
@@ -34,5 +42,12 @@ public class Course {
 
     public void setName(String name) {
         this.name = name;
+    }
+    public List<Wizard> getWizards() {
+        return wizards;
+    }
+
+    public void setPotions(List<Wizard> wizards) {
+        this.wizards = wizards;
     }
 }
